42eccdd6d119e2031243fb31a5eec6967ddc1493
Slava <slavatimchbdxueh@gmail.com>
2024-12-13 20:03:55 +0500

    3.12 платформы

--- platform.py
+++ platform.py
@@ -1,3 +1,7 @@
+from pydoc_data.topics import topics
+from sys import platform
+import img
+
 import pygame
 import const
 
@@ -5,9 +9,27 @@ class Platform(pygame.sprite.Sprite):
     def __init__(self, x, y, width, height):
         super().__init__()
         self.image = pygame.image.load("img/platform.png")
-        self.image = pygame.transform.scale(self.image, [width, height])
+        self.image = pygame.transform.scale(self.image, [self.image.get_width(), self.image.get_height()])
+        self.image = self.scale_image(self.image, 80)
         self.rect = self.image.get_rect()
         self.rect.topleft = [x, y]
+        self.width = width
+        self.height = height
 
     def update(self):
-        pass
+        platform_surface = pygame.Surface((self.width, self.height))
+        for x in range(0, self.width, self.image.get_width()):
+            y = 0
+            platform_surface.blit(self.image, (x, y))
+
+        self.image = platform_surface
+        self.rect = self.image.get_rect(topleft=self.rect.topleft)
+
+
+    def scale_image(self, img, max_width):
+        orig_w = img.get_width()
+        orig_h = img.get_height()
+        ratio = orig_w / orig_h
+        new_w = max_width
+        new_h = new_w / ratio
+        return pygame.transform.scale(img, (new_w, new_h))
